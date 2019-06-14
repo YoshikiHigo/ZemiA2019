74daa482a7a8e8afc4478f2f17e24bbb2451c082
Kohei <nayuta222103@gmail.com>
2019-06-14 13:34:23 +0900

    こめんと追加

--- src/main/java/zemiA/ZemiAMain.java
+++ src/main/java/zemiA/ZemiAMain.java
@@ -4,6 +4,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTParser;
@@ -15,24 +16,31 @@ public class ZemiAMain {
     List<String> lines = null;
     try {
       lines = Files.readAllLines(Paths.get("src/main/java/zemiA/ZemiAMain.java"),
-          StandardCharsets.ISO_8859_1);
+          StandardCharsets.ISO_8859_1); //全ての行を読み込む
     } catch (final Exception e) {
       System.err.println(e.getMessage());
       return;
     }
 
-    final ASTParser parser = ASTParser.newParser(AST.JLS11);
+   final ASTParser parser = ASTParser.newParser(AST.JLS11); //ASTパーサ（構文解析木）の作成
+
+   //各行の要素をjoinしてパーサにセット
     parser.setSource(String.join(System.lineSeparator(), lines).toCharArray());
 
     CompilationUnit unit = null;
     try {
+    	//パーサを使ってこのソースコードの構文木を作る
       unit = (CompilationUnit) parser.createAST(new NullProgressMonitor());
+
     } catch (final Exception e) {
       System.err.println(e.getMessage());
       return;
     }
 
+    //ビジター... 木構造を辿っていくクラスのインスタンスの定義
     final ZemiAVisitor visitor = new ZemiAVisitor();
+
+    //引数のビジターのインスタンスで構文抽象木をたどる
     unit.accept(visitor);
   }
 }
--- src/main/java/zemiA/ZemiAVisitor.java
+++ src/main/java/zemiA/ZemiAVisitor.java
@@ -3,17 +3,18 @@ package zemiA;
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
 import org.eclipse.jdt.core.dom.SimpleName;
-import org.eclipse.jdt.core.dom.Statement;
 
 public class ZemiAVisitor extends ASTVisitor {
 
   @Override
+  //引数がノードの種類(SimpleName)
   public boolean visit(SimpleName node) {
-    System.out.println(node.getIdentifier());
+    System.out.println(node.getIdentifier()); //ノードの名前を標準出力に出す
     return super.visit(node);
   }
 
-  @Override
+
+@Override
   public boolean visit(MethodDeclaration node) {
     // System.out.println(node.toString());
     return super.visit(node);
