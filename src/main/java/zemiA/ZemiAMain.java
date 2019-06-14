fa15e55a2c2e15bbd27467d60ae04f12111017f7
wolsnak <37971918+wolsnak@users.noreply.github.com>
2019-06-14 13:32:11 +0900

    add int.

--- src/main/java/zemiA/ZemiAMain.java
+++ src/main/java/zemiA/ZemiAMain.java
@@ -4,6 +4,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTParser;
@@ -20,7 +21,8 @@ public class ZemiAMain {
       System.err.println(e.getMessage());
       return;
     }
-
+    int abc = 0;
+    abc++;
     final ASTParser parser = ASTParser.newParser(AST.JLS11);
     parser.setSource(String.join(System.lineSeparator(), lines).toCharArray());
 
